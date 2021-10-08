cd8cc0b8b7d8828bf08f7896acdd9c7fe8817439
SantiagoRamirezA01254173 <A01254173@itesm.mx>
2021-10-07 21:43:40 -0700

    ptm no hice branch pero a ver si funciona igual

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 print("Ei que ondas mundo. Mi contraseña es 12345ABC")
 while True:
     bruh = input("(S)i o (N)o raza??? ")
-    if bruh == "S":
+    if bruh.upper() == "S":
         print("Jaja asies")
         break
-    elif bruh == "N":
+    elif bruh.upper() == "N":
         print("Como que no hdtpm?")
         break
+
+print("\nptm no hice branch pero a ver si funciona igual")
