a3fb9969f4e5396bf2a1894faea2e44a70be4799
SantiagoRamirezA01254173 <A01254173@itesm.mx>
2021-10-07 21:39:51 -0700

    ptm no hice branch

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Ei que ondas mundo. Mi contraseña es 12345ABC")
+while True:
+    bruh = input("(S)i o (N)o raza??? ")
+    if bruh == "S":
+        print("Jaja asies")
+        break
+    elif bruh == "N":
+        print("Como que no hdtpm?")
+        break
